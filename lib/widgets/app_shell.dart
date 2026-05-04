5176c7c3849e957f4151642287a9927b40c06e17
Vortex-VRX <dodo70756@gmail.com>
2026-05-03 21:23:47 -0400

    Use shared tab state for navigation

--- lib/widgets/app_shell.dart
+++ lib/widgets/app_shell.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../core/app_state.dart';
 import '../screens/cart/cart_screen.dart';
 import '../screens/home/home_screen.dart';
 import '../screens/profile/profile_screen.dart';
@@ -14,8 +15,6 @@ class GroceryPriceCompareApp extends StatefulWidget {
 }
 
 class _GroceryPriceCompareAppState extends State<GroceryPriceCompareApp> {
-  int _currentIndex = 0;
-
   late final List<Widget> _screens = [
     const HomeScreen(),
     const SearchScreen(),
@@ -26,20 +25,23 @@ class _GroceryPriceCompareAppState extends State<GroceryPriceCompareApp> {
 
   @override
   Widget build(BuildContext context) {
+    final appState = AppStateScope.of(context);
+    final currentIndex = appState.currentTabIndex;
+
     return Scaffold(
-      body: IndexedStack(index: _currentIndex, children: _screens),
+      body: IndexedStack(index: currentIndex, children: _screens),
       bottomNavigationBar: DecoratedBox(
         decoration: const BoxDecoration(
           color: Colors.white,
           border: Border(top: BorderSide(color: Color(0xFFE4E7EC))),
         ),
         child: NavigationBar(
-          selectedIndex: _currentIndex,
-          onDestinationSelected: (index) => setState(() => _currentIndex = index),
+          selectedIndex: currentIndex,
+          onDestinationSelected: appState.selectTab,
           destinations: const [
             NavigationDestination(icon: Icon(Icons.home_outlined), selectedIcon: Icon(Icons.home), label: 'Shop'),
             NavigationDestination(icon: Icon(Icons.search_outlined), selectedIcon: Icon(Icons.search), label: 'Search'),
-            NavigationDestination(icon: Icon(Icons.shopping_cart_outlined), selectedIcon: Icon(Icons.shopping_cart), label: 'Cart'),
+            NavigationDestination(icon: Icon(Icons.compare_arrows_outlined), selectedIcon: Icon(Icons.compare_arrows), label: 'Compare'),
             NavigationDestination(icon: Icon(Icons.storefront_outlined), selectedIcon: Icon(Icons.storefront), label: 'Stores'),
             NavigationDestination(icon: Icon(Icons.person_outline), selectedIcon: Icon(Icons.person), label: 'You'),
           ],
