287a324535ed5227116dd155067c31106c7ddc01
Vortex-VRX <dodo70756@gmail.com>
2026-05-10 21:29:23 -0400

    Use Supabase publishable key by default

--- lib/core/supabase_config.dart
+++ lib/core/supabase_config.dart
@@ -4,7 +4,10 @@ class SupabaseConfig {
     defaultValue: 'https://npnekplfuwnjlqvzwfdl.supabase.co',
   );
 
-  static const anonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
+  static const anonKey = String.fromEnvironment(
+    'SUPABASE_ANON_KEY',
+    defaultValue: 'sb_publishable_A8PrphNrGB1A85OCXx-4LA_9WnEPG1H',
+  );
 
   static bool get isConfigured => url.isNotEmpty && anonKey.isNotEmpty;
 }
